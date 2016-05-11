ab54f51bd53fe52e5142cef84d603588746d9f73
lorashopova <lshopowa@yahoo.com>
2016-05-11 11:05:56 +0300

    HTML Tables Homework
    HTML Homework

---
+++ HTMLTables/04.Calculator/script.js
@@ -0,0 +1,22 @@
+﻿var display = document.getElementById('display');
+var calcButtons = document.getElementsByClassName('calcButtons');
+var clear = document.getElementById('clear');
+var evalButton = document.getElementById('evalButton');
+
+for (var i = 0; i < calcButtons.length; i++) {
+    calcButtons[i].addEventListener('click', function (e) {
+        var target = e.target;
+        var input = target.innerHTML;
+        display.value += input;
+    }, false);
+}
+
+clear.addEventListener('click', function () {
+    display.value = ' ';
+}, false);
+
+evalButton.addEventListener("click", function () {
+    if (display.value != '') {
+        display.value = eval(display.value);
+    }
+}, false);
