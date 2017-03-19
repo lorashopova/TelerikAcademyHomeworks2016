6d8ff05f3ed09e8e6c24912c32e35859ae216884
lorashopova <lshopowa@yahoo.com>
2017-03-19 21:25:04 +0200

    JavaScript UI&DOM Homework
    JavaScript UI&DOM Homework

---
+++ JavaScriptUI&DOMHomeworks/Handlebars/tasks/task-3.js
@@ -0,0 +1,25 @@
+function solve() {
+  return function () {
+    $.fn.listview = function (data) {
+      var element,
+        templateSourceID,
+        source,
+        template,
+        length = data.length,
+        item;
+
+      element = $(this);
+      templateSourceID = element.attr('data-template');
+      source = $('#' + templateSourceID).html();
+      template = handlebars.compile(source); 
+
+      for (item = 0; item < length; item += 1) {
+        element.append(template(data[item]));
+      }
+
+      return this;
+    };
+  };
+}
+
+module.exports = solve;
